5feb11641467e9c8f31310391128fc4ecffd6e41
Hossien.S <h.soli.jff@gmail.com>
2025-05-10 17:53:54 +0330

    a fresh spring-boot application

---
+++ src/main/java/dev/hspl/taskbazi/TaskbaziApplication.java
@@ -0,0 +1,13 @@
+package dev.hspl.taskbazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskbaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskbaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hspl/taskbazi/TaskbaziApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hspl.taskbazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskbaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
